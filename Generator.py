25f49f0a958982f042e185d94d2ed6155d73b730
AK-PK22 <129682500+AK-PK22@users.noreply.github.com>
2023-06-09 12:43:27 +0200

    Update Generator.py

--- Generator.py
+++ Generator.py
@@ -1,1 +1,13 @@
-# PortscannerPy
+#Passwort-Generator
+import random
+
+lower_case = "abcdefghijklmnopqrstuvwxyz" #Kleine Buchstaben
+upper_case = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"#Große Buchstaben
+num = "0123456789"
+symbol = "[]{}#()*;._-"
+
+ans = lower_case + upper_case + num + symbol
+
+lenght = 10
+password = "".join(random.sample(ans, lenght))
+print("Passwort wird generiert... Das passwort lautet: ",password)
