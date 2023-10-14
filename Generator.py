c327dbaaa092be8e53e4ebd8e54250a7c75b68c7
Peyman Kinz <129682500+Peyman-Kinz@users.noreply.github.com>
2023-10-14 11:55:01 +0200

    Update Generator.py

--- Generator.py
+++ Generator.py
@@ -1,12 +1,46 @@
 import random
+import string
+import time
 
-lower_case = "abcdefghijklmnopqrstuvwxyz" 
-upper_case = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-num = "0123456789"
-symbol = "[]{}#()*;._-"
+def generate_lowercase():
+    return random.choice(string.ascii_lowercase)
 
-ans = lower_case + upper_case + num + symbol
+def generate_uppercase():
+    return random.choice(string.ascii_uppercase)
 
-lenght = 10
-password = "".join(random.sample(ans, lenght))
-print("Passwort wird generiert... Das passwort lautet: ",password)
+def generate_digits():
+    return random.choice(string.digits)
+
+def generate_symbols():
+    symbols = "[]{}#()*;._-"
+    return random.choice(symbols)
+
+def generate_password():
+    password = [
+        generate_lowercase(),
+        generate_uppercase(),
+        generate_digits(),
+        generate_symbols()
+    ]
+
+    remaining_length = 10 - len(password)
+    all_characters = string.ascii_letters + string.digits + "[]{}#()*;._-"
+    password.extend(random.choices(all_characters, k=remaining_length))
+    random.shuffle(password)
+
+    return ''.join(password)
+
+def display_loading_screen():
+    print("Wird generiert...")
+    time.sleep(4)  # Simulate a loading delay
+
+def display_password(password):
+    print("Das Passwort lautet:", password)
+
+def main():
+    display_loading_screen()
+    generated_password = generate_password()
+    display_password(generated_password)
+
+if __name__ == "__main__":
+    main()
